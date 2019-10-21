6259c545107a9adeeeb526f5e6606bc5af0747cd
ianhunterharold <ianhunterharold@gmail.com>
2019-10-21 13:15:45 -0700

    working on adding a fetch to a math api, not currently functional

--- escaperoom-frontend/js/ian_js.js
+++ escaperoom-frontend/js/ian_js.js
@@ -2,9 +2,13 @@ document.addEventListener('DOMContentLoaded', (event)=>{
   main();
 })
 
+const mathURL = 'http://numbersapi.com/random/math'
+
 function main(){
   addButton();
-  buttonAction();
+  addImage();
+  firstbuttonAction();
+  goGetMathQuestion();
 }
 
 function addButton(){
@@ -15,13 +19,22 @@ function addButton(){
   lowerDiv.append(button);
 }
 
-function buttonAction(){
+function addImage(){
+  let imageAdded = document.createElement('img');
+  imageAdded.setAttribute('src','./images/entrance_flatiron.jpg');
+  imageAdded.setAttribute('id','image-specific-id');
+  imageAdded.setAttribute('alt','flatiron seattle school entrance');
+  let imageLocation = document.getElementById('top-div');
+  imageLocation.append(imageAdded)
+}
+
+function firstbuttonAction(){
 
   function firstDelayedClue(){
-    let firstClue = window.alert("I have bark, but I am not a dog");
+    let firstClue = window.alert("Clue 1 - I have bark, but I am not a dog");
     firstClue;
   }
-  window.setTimeout(firstDelayedClue,3000);
+  window.setTimeout(firstDelayedClue,4000);
 
   let button = document.getElementById("button");
   button.addEventListener('mouseenter', (event) =>{
@@ -32,25 +45,49 @@ function buttonAction(){
       window.alert("good job!")
       let secondScreen = window.prompt("A list is the same as a queue","true or false?");
       if (secondScreen === "false"){
-        window.alert("nice, two for two!")
+        window.alert("Nice work!\n Clue 2\n - Azure is my friend\n Netflix and chill all day\n Living-room needs me ")
+        secondButtonCreated();
       } else {
         window.alert("almost")
       }
-       
     } else {
       window.alert("close but no cigar");
     }
-
-    // if statement chain is all messed up, seek help from kara and brian.
-    // if(secondScreen ==="false"){
-    //   window.alert("two for two!")
-    // } else {
-    //   window.alert("not quite");
-    // }
-
-
-
   })
-
-
 }
+
+function secondButtonCreated(){
+  //created once you passed the two challenges 
+  let secondButton = document.createElement('button');
+  secondButton.setAttribute('id','second-button-id');
+  let lowerDiv = document.getElementById('lower-div');
+  lowerDiv.append(secondButton);
+  secondButtonAction();
+}
+
+function goGetMathQuestion(){
+  fetch(mathURL, {
+    headers: {
+      "Accept" : 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(mathFact => {
+    console.log(mathFact);
+    // displayMathQuestion(mathFact)
+  
+});
+}
+
+function secondButtonAction(){
+  let secondButtonId = document.getElementById('second-button-id');
+  secondButtonId.addEventListener('mouseenter', (event)=> {
+    
+
+  });
+}
+
+// function displayMathQuestion(mathFact){
+//   console.log("are we even here yet?", mathFact);
+
+// }
