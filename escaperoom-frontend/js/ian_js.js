19426d17ac743bc3e7ce96ba46658ebff643130c
ianhunterharold <ianhunterharold@gmail.com>
2019-10-21 11:52:11 -0700

    fixed nested if statement for challenge questions to flow appropriately

--- escaperoom-frontend/js/ian_js.js
+++ escaperoom-frontend/js/ian_js.js
@@ -26,18 +26,30 @@ function buttonAction(){
   let button = document.getElementById("button");
   button.addEventListener('mouseenter', (event) =>{
     let firstScreen = window.prompt("First Challenge! Which is best for creating constant variables? Var, let or const?","tick tock, tick tock.");
-    let secondScreen = window.prompt("a list is the same as a queue","true or false?");
+  
+    
     if (firstScreen === "const"){
       window.alert("good job!")
+      let secondScreen = window.prompt("A list is the same as a queue","true or false?");
+      if (secondScreen === "false"){
+        window.alert("nice, two for two!")
+      } else {
+        window.alert("almost")
+      }
+       
     } else {
       window.alert("close but no cigar");
     }
+
     // if statement chain is all messed up, seek help from kara and brian.
-    if(secondScreen ==="false"){
-      window.alert("two for two!")
-    } else {
-      window.alert("not quite");
-    }
+    // if(secondScreen ==="false"){
+    //   window.alert("two for two!")
+    // } else {
+    //   window.alert("not quite");
+    // }
+
+
+
   })
 
 
