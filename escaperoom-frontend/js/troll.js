ab7b1f9fd8f02e4465e50b8cafb6b4c4f81da80c
kara-o <44376276+kara-o@users.noreply.github.com>
2019-10-22 15:24:38 -0700

    about to merge

--- escaperoom-frontend/js/troll.js
+++ escaperoom-frontend/js/troll.js
@@ -1,29 +1,35 @@
-function trollPuzzle() {
+function trollPuzzleStart() {
 
   const trollWords = document.getElementById('troll-words')
   const trollPuzzleContainer = document.getElementById('troll-puzzle-container')
   const trollImg = document.getElementById('fremont-troll-image')
-  const trollBtns = document.getElementById('troll-btns')
+  const trollWordBox = document.getElementById('left-dialog-box')
+  trollWordBox.appendChild(trollWords)
 
   trollPuzzleContainer.style.display = "block";
   trollImg.style.display = "block"
 
   setTimeout(function () {
-    trollWords.textContent = "I know where you have to go next."
+    trollWords.textContent = "You found me!"
   }, 1000);
   setTimeout(function () {
-    trollWords.textContent = "But first..."
+    trollWords.textContent = "I know where you have to go next."
   }, 2000);
   setTimeout(function () {
-    trollWords.textContent = "You have to correctly answer my questions!"
+    trollWords.textContent = "But first..."
   }, 3000);
+  setTimeout(function () {
+    trollWords.textContent = "You have to correctly answer my questions!"
+  }, 4000);
   setTimeout(function () {
     trollWords.textContent = "";
     trollQuestions()
-  }, 4000);
+  }, 5000);
 
 }
 
 function trollQuestions() {
+
+  const
 
 }
