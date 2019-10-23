bcb5516a3d9217e95fedc899d226573e5876ca9e
kara-o <44376276+kara-o@users.noreply.github.com>
2019-10-23 15:10:34 -0700

    completed troll challenge

--- escaperoom-frontend/js/troll.js
+++ escaperoom-frontend/js/troll.js
@@ -1,10 +1,12 @@
+const trollWordBox = document.getElementById('left-dialog-box')
+const trollPuzzleContainer = document.getElementById('troll-puzzle-container')
+
 function trollPuzzleStart() {
-  const trollPuzzleContainer = document.getElementById('troll-puzzle-container')
   trollPuzzleContainer.style.display = "block"
 
   const trollWords = document.createElement('p')
+  trollWords.id = "troll-words"
   const trollImg = document.getElementById('fremont-troll-image')
-  const trollWordBox = document.getElementById('left-dialog-box')
   trollWordBox.style.display = "block"
   trollWordBox.appendChild(trollWords)
   trollImg.style.display = "block"
@@ -24,11 +26,73 @@ function trollPuzzleStart() {
   // }, 6000);
   setTimeout(function () {
     trollWords.textContent = "";
-    trollQuestions()
+    trollChallenge(jokes, answers)
   }, 4000);
 
 }
 
-function trollChallenge() {
+const jokes = ["What's the object-oriented way to become wealthy?", "Why did the programmer quit his job?"]
+const answers = ["Inheritance", "Because he didn't get arrays."]
 
+function trollChallenge(jokes, answers) {
+  const jokeForm = document.createElement('form')
+  jokeForm.id = 'joke-form'
+  const jokeAnswer = document.createElement('input')
+  jokeAnswer.setAttribute("type", "text")
+  jokeAnswer.name = 'answer'
+  const jokeSubmit = document.createElement('input')
+  jokeSubmit.setAttribute("type", "submit")
+  trollWordBox.appendChild(jokeForm)
+  jokeForm.append(jokeAnswer, jokeSubmit)
+  const jokeFeedback = document.createElement("p")
+  trollWordBox.appendChild(jokeFeedback)
+  jokeFeedback.textContent = ""
+
+  let i = 0
+  while (i < jokes.length) {
+  trollWords.textContent = jokes[i]
+
+  jokeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let input = event.target.elements.answer.value
+    if (i = 0) {
+      if (input.toLowerCase() === answers[i].toLowerCase()) {
+        jokeFeedback.textContent = "Good job!"
+        setTimeout(() => {
+          jokeFeedback.textContent = ""
+        }, 2000)
+      }
+      else {
+        jokeFeedback.textContent = "Nope, keep trying!"
+        setTimeout(() => {
+          jokeFeedback.textContent = ""
+        }, 2000)
+      }
+    }
+    else {
+      if (input.toLowerCase().includes('arrays')) {
+        jokeFeedback.textContent = "Good job!"
+        setTimeout(() => {
+          jokeFeedback.textContent = ""
+        }, 2000)
+        let trollWords = document.getElementById("troll-words")
+        trollWords.textContent = "Wow, you have brains and a sense of humor.  Here are your Ruby gems..."
+        generateRubies();
+        gameWon();
+      }
+      else {
+        jokeFeedback.textContent = "Nope, keep trying!"
+        setTimeout(() => {
+          jokeFeedback.textContent = ""
+        }, 2000)
+      }
+    }
+    i++
+  })
+ }
+}
+
+function clearTrollPuzzle() {
+  trollPuzzleContainer.style.display = "none";
+  trollWordBox.style.display = "none";
 }
