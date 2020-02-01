65b60562d0b9fc88b7fd7613b61956b9968d5610
kara-o <44376276+kara-o@users.noreply.github.com>
2020-02-01 13:11:52 -0800

    downloaded moment.js, updated score format

--- escaperoom-frontend/js/clock.js
+++ escaperoom-frontend/js/clock.js
@@ -1,27 +1,41 @@
-
-
 function startTimer() {
-  const timer = document.getElementById("timer");
-  timer.textContent = 600
+  const timer = document.getElementById('timer');
+  timer.textContent = '5:00';
   setInterval(() => {
     decreaseTimer();
   }, 1000);
 }
 
 function decreaseTimer() {
-  const timer = document.getElementById("timer");
-  time = timer.textContent;
-  timeNum = parseInt(time, 10);
-  timeNum -= 1;
-  if (timeNum === 0) {
+  const timer = document.getElementById('timer');
+  let time = timer.textContent;
+  let digits = time.split(':'); // ['5', '00']
+  let minutes = parseInt(digits[0], 10);
+  let seconds = parseInt(digits[1], 10);
+
+  if (time == '0:00') {
     loseGame();
+  } else if (seconds <= 10 && seconds > 0) {
+    new_seconds = seconds - 1;
+    seconds = '0' + new_seconds.toString();
+  } else if (seconds > 10 && seconds <= 59) {
+    seconds -= 1;
+    seconds = seconds.toString();
+  } else if (seconds === 0) {
+    minutes -= 1;
+    minutes = minutes.toString();
+    seconds = '59';
   }
-  timer.textContent = timeNum;
+
+  time = minutes + ':' + seconds;
+  timer.textContent = time;
 }
 
 function stopTimer() {
-  document.getElementById("timer").style.display = "none";
-  console.log('TIMER STOPPED, GAME WON!!! Last successful puzzle completion will call this timer')
+  document.getElementById('timer').style.display = 'none';
+  console.log(
+    'TIMER STOPPED, GAME WON!!! Last successful puzzle completion will call this timer'
+  );
 }
 
 //define loseGame
