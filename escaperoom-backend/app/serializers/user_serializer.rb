24d8e065648f4f6de44b243b500ffd873299f1d4
Kara Odle <karajo.odle@gmail.coml>
2020-02-05 01:18:47 -0800

    minor display changes to dialog boxes, added viewScores to login

--- escaperoom-backend/app/serializers/user_serializer.rb
+++ escaperoom-backend/app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :scores
+
+  # def sorted_scores
+  #   sorted_scores = Score.where(user_id: self.object.id).order(created_at: :desc)
+  #   byebug
+  # end
+
 end
